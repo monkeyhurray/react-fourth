42adf315b8978c8a1b7ae132a511536651ffc467
sangho <keumm9635@gmail.com>
2024-03-05 11:09:53 +0900

    fix:css 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,16 @@ import {
   confirmPatchTodo,
   cancelPatchTodo,
 } from "../api/todo";
+import {
+  Main,
+  Input,
+  ContentDiv,
+  H2Title,
+  SemiContentDiv,
+  ButtonDiv,
+  ConfirmBtn,
+} from "../styles/StyledHome";
 import uuid from "react-uuid";
-import styled from "styled-components";
 
 interface Todo {
   id: string;
@@ -132,42 +140,3 @@ const Home = () => {
 };
 
 export default Home;
-
-const Main = styled.div`
-  margin: 0 auto;
-  display: block;
-  width: fit-content;
-  max-width: 1200px;
-  max-height: 800px;
-`;
-const Input = styled.input`
-  margin-top: 20px;
-  margin-right: 5px;
-  align-items: center;
-  justify-content: center;
-`;
-const ContentDiv = styled.div`
-  padding: 5px;
-  display: inline-block;
-  width: 210px;
-  height: 130px;
-  border-radius: 5px;
-  border-color: black;
-  border: 1px solid black;
-  margin: 5px 13px 5px 0px;
-`;
-const H2Title = styled.h1`
-  margin: 10px;
-`;
-const SemiContentDiv = styled.div`
-  margin-top: 5px;
-  margin-bottom: 10px;
-`;
-
-const ButtonDiv = styled.div`
-  margin-top: 5px;
-  float: right;
-`;
-const ConfirmBtn = styled.button`
-  margin: 2px;
-`;
---
+++ src/styles/StyledHome.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  margin: 0 auto;
+  display: block;
+  width: fit-content;
+  max-width: 1200px;
+  max-height: 800px;
+`;
+export const Input = styled.input`
+  margin-top: 20px;
+  margin-right: 5px;
+  align-items: center;
+  justify-content: center;
+`;
+export const ContentDiv = styled.div`
+  padding: 5px;
+  display: inline-block;
+  width: 210px;
+  height: 130px;
+  border-radius: 5px;
+  border-color: black;
+  border: 1px solid black;
+  margin: 5px 13px 5px 0px;
+`;
+export const H2Title = styled.h1`
+  margin: 10px;
+`;
+export const SemiContentDiv = styled.div`
+  margin-top: 5px;
+  margin-bottom: 10px;
+`;
+
+export const ButtonDiv = styled.div`
+  margin-top: 5px;
+  float: right;
+`;
+export const ConfirmBtn = styled.button`
+  margin: 2px;
+`;
